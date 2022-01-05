b17fffb7e587cb20787ae2fda94ccb8a5e106cde
SmitaDugur <smitadugur25@gmail.com>
2022-01-05 05:47:41 +0530

    ADDED:initial commit

---
+++ src/com/bridgelabzregex/LastName.java
@@ -0,0 +1,26 @@
+package com.bridgelabzregex;
+
+import java.util.Scanner;
+
+public class LastName {
+	public static void main(String[] args) {
+	  Scanner scanner  = new Scanner(System.in);
+      RegexRegistration regex = new RegexRegistration();
+   
+ 
+	  System.out.println("Enter your FirstName:");
+	 
+	  String firstName = scanner.next(); 
+	  regex.userFirstName(firstName);
+	  
+	  System.out.println("Enter your LastName:");
+
+	  String lastName = scanner.next(); 
+	  regex.userFirstName(lastName);
+
+
+	  
+	}
+	
+}
+© 2022 GitHub, Inc.
